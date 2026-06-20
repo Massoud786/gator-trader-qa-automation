3a48376cbe1643f7ae3e6d52eec85a78a2a75014
Massoud786 <massoud152.homayoun@gmail.com>
2026-06-20 11:52:23 -0700

    Implement framework base setup and configuratoin utilities

--- Automation-Testing/src/test/java/com/gatortrader/qa/runner/Test.java
+++ Automation-Testing/src/test/java/com/gatortrader/qa/runner/Test.java
@@ -1,4 +1,20 @@
 package com.gatortrader.qa.runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "",
+        dryRun = false,
+        plugin = {
+                "pretty",
+                "html:target/cucumber-html-report.html",
+                "json:target/cucumber.json",
+                "junit:target/cucumber.xml"
+        }
+)
 public class Test {
+
 }
--- Automation-Testing/src/test/java/com/gatortrader/qa/utility/SeleniumUtilities.java
+++ Automation-Testing/src/test/java/com/gatortrader/qa/utility/SeleniumUtilities.java
@@ -1,4 +1,32 @@
 package com.gatortrader.qa.utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.gatortrader.qa.base.BaseSetup;
+import java.time.Duration;
 
-public class SeleniumUtilities {
+public class SeleniumUtilities extends BaseSetup {
+    private WebDriverWait getWait(){
+        return new WebDriverWait(getChrome(), Duration.ofSeconds(20));
+    }
+    public void clickOnElement(By locator){
+        getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+    public String getElementText(By locator)  {
+
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+    public void sentTextToElement(By locator, String text){
+        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
+    }
+    public boolean isElementDisplayed(By locator){
+        try {
+            return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).isDisplayed();
+        }
+        catch (TimeoutException e){
+            return false;
+        }
+
+    }
 }
